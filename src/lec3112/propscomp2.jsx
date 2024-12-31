eb5c62adec80c2abc273dd37397dafcfe35f0643
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-12-31 15:14:17 +0530

    t2

--- src/lec3112/propscomp2.jsx
+++ src/lec3112/propscomp2.jsx
@@ -12,3 +12,12 @@ class MyPropsList extends React.Component {
   }
 }
 export default MyPropsList;
+
+// import * as React from "react";
+// class MyPropButton extends React.Component {
+//   render() {
+//     const { xyz, abc } = this.props;
+//     return <button disabled={xyz}>{abc}</button>;
+//   }
+// }
+// export default MyPropButton;

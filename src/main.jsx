025c127c44a0b759d0d7d8db80a32240de56fd3c
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-12-31 15:41:23 +0530

    t2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Xyz from "./lec2412/component1";
-import PropExmp from "./lec3112/propscomp3";
+import Defaultprops from "./lec3112/defaultprops";
 
 // 1. ClassBased implementation
 // 2. Function Based implementation
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PropExmp />
+    <Defaultprops />
+    <br />
+    <Defaultprops var1={true} />
+    <br />
+    <Defaultprops var2={"yellow"} />
+    <br />
+    <Defaultprops var3={"New Text"} />
+    <br />
+    <Defaultprops var1={true} var2={"red"} />
+    <br />
+    <Defaultprops var1={true} var2={"blue"} var3={"abc"} />
   </StrictMode>
 );
